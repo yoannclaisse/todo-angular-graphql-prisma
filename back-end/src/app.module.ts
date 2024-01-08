ce72f5f4ab940aa3236786f3efdb9cb0758d86e3
yoannclaisse <claisse.yoann@gmail.com>
2024-01-08 14:53:21 +0100

    modify architecture

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// Ce module est importer directement car j'ai utiliser le cli de nest
 import { GraphqlModule } from './graphql/graphql.module';
 
 @Module({
