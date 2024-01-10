de622dcb46f5579727ed6fa15ec0c679e6093cec
yoannclaisse <claisse.yoann@gmail.com>
2024-01-10 17:45:57 +0100

    add todo to user subscribe

--- front-end/src/app/user/user.component.ts
+++ front-end/src/app/user/user.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { GraphqlService  } from '../graphql.service';
-import { TodosQueryResponse, UserQueryResponse } from '../models/graphql.model';
+import { Todo, TodosQueryResponse, UserQueryResponse } from '../models/graphql.model';
 import { ApolloQueryResult } from '@apollo/client/core';
 import { Apollo } from 'apollo-angular';
 @Component({
@@ -12,16 +12,27 @@ import { Apollo } from 'apollo-angular';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  resultStatus: String = ""
+  todos: Array<Todo> = []
   constructor(private GraphqlService: GraphqlService) {}
   // Function onclick button
   userTodos(userValue: String) {
     // si le champs est vide rien ne se passe
+    this.resultStatus = ""
     if(!userValue) {
+      this.resultStatus = "please enter a name"
       return
     }
     this.GraphqlService
       .getUserTodos(userValue).subscribe((result: ApolloQueryResult<UserQueryResponse>) => {
         console.log('Result : ', result)
+        const user = result.data.user
+        if(!user) {
+          this.resultStatus = "user not found"
+          return
+        }
+        this.resultStatus = `welcome : ${user.username}` 
+        this.todos = user.todos
       })
   }
 
