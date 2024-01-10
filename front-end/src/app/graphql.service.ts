d28a2e0e3e4eb1cb80d6864a3b06f2bbe24d8fd6
yoannclaisse <claisse.yoann@gmail.com>
2024-01-10 15:07:18 +0100

    add graphql service

---
+++ front-end/src/app/graphql.service.ts
@@ -0,0 +1,10 @@
+// Ce service va servir à faire toutes les requêtes graphql
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GraphqlService {
+
+  constructor() { }
+}
