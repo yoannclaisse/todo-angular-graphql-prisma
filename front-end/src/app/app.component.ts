c0fe9aac71a57cd523129c3cbbb37fb0e7d83a43
yoannclaisse <claisse.yoann@gmail.com>
2024-01-10 14:48:31 +0100

    add and import user component

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// import du component user
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- front-end/src/app/app.config.ts
+++ front-end/src/app/app.config.ts
@@ -3,6 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// faire un import de provideHttpClient car le client graphQL va e=xecuter des requêtes HTTP
+import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient()]
 };
