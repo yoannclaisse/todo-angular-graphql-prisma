dd407780f7e15cb0fee594302c82129837599079
yoannclaisse <claisse.yoann@gmail.com>
2024-01-18 15:21:37 +0100

    add unit test

--- front-end/src/app/user/user.component.spec.ts
+++ front-end/src/app/user/user.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserComponent } from './user.component';
+import { GraphqlService } from '../graphql.service';
+import { Apollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -8,7 +12,8 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserComponent]
+      imports: [UserComponent],
+      providers: [GraphqlService, Apollo, HttpClient, HttpHandler]
     })
     .compileComponents();
     
